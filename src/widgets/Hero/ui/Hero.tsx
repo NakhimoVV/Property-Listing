ff4d5cd0d2ecf5c7cc1c89fbb694961294a84a03
NakhimoVV <tj1993@bk.ru>
2025-09-09 17:49:23 +0300

    fix Hero.scss

--- src/widgets/Hero/ui/Hero.tsx
+++ src/widgets/Hero/ui/Hero.tsx
@@ -9,8 +9,8 @@ const Hero = (props: HeroProps) => {
     <section className="hero">
       <div className="hero__pano">
         <div className="hero__pano-inner">
-          <h1>Peace, nature, dream</h1>
-          <p>Find and book a great experience</p>
+          <h1 className='hero__title'>Peace, nature, dream</h1>
+          <p className='hero__subtitle'>Find and book a great experience</p>
         </div>
       </div>
     </section>
